ea18be9d95a8b1e36b1218553828762c52ec13e3
donyamardani <zmardani0@gmail.com>
2025-11-07 23:47:29 +0330

    create slider component with js&react
    create slider component with js&react

--- src/Components/Arrow.jsx
+++ src/Components/Arrow.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Arrow() {
   return (
-    <div>Arrow</div>
+   
+            <div class="arrow-wrapper">
+                <div class="round">
+                    <div id="cta">
+                        <span class="arrow prev"></span>
+                    </div>
+                </div>
+            </div>
   )
 }
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Arrow from './Arrow'
+import Card from './Card'
 
 export default function Slider() {
+    const [heros,setHeros]=useState();
+    const [curentHeros,setCurentHeros]=useState();
+
+    useEffect(
+        ()=>{
+            (
+            async()=>{
+                const res=await fetch('http://localhost:3001/heroes')
+                const data=await res.json()
+                setHeros(data)
+            }
+            )()
+        },[]
+    );
+    const nextCurent=()=>{
+       setCurentHeros(curentHeros==heros?.length-1?0:curentHeros+1)
+    }
+    const prevCurent=()=>{
+       setCurentHeros(curentHeros==0?heros?.length-1:curentHeros-1)
+    }
   return (
-    <div>Slider</div>
+   
+    <div class="slide-container">
+       
+        <div class="wrapper">
+
+          <Arrow/>
+          <Card/>
+          <Arrow/>
+
+        </div>
+     </div>
   )
 }
